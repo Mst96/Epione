fd9238e6f5e6cb44f2ccb884bae679cc067ac6df
Osas <matthiasobazee@gmail.com>
2017-11-05 20:40:43 +0000

    Base for firebase connection

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class FirebaseController extends Controller
+{
+    protected $serviceAccount;
+    protected $firebase;
+    protected $database;
+
+    public function __construct()
+    {
+        $this->serviceAccount = ServiceAccount::fromJsonFile('../.epione-6b357-firebase-adminsdk-yttnz-7acd790a9d.json');
+
+        $this->firebase = (new Factory)
+            ->withServiceAccount($this->serviceAccount)
+            ->create();
+
+        $this->database = $this->firebase->getDatabase();
+    }
+
+    public function index()
+    {
+        $reference = $this->database->getReference('');
+        $value = $reference->getValue();
+
+        var_dump($this->serviceAccount);
+        var_dump($this->firebase);
+        var_dump($this->database);
+        var_dump($reference);
+        var_dump($value);
+
+        // $this->database->getReference('Carers/1/lastName')->set('Brodious');       
+        
+        $this->database->getReference('Carers/8')
+            ->set([
+                    'firstName' => 'Michael',
+                    'lastName' => 'Bolton',
+                ]);
+
+// $db->getReference('config/website/name')->set('New name');
+
+        return response()->json($value);        
+    }
+    
+    public function set($value='')
+    {
+        # code...
+    }
+}
