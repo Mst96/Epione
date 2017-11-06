d1fe747e874f0aff906d4473120aaf79ed62974c
Osas <matthiasobazee@gmail.com>
2017-11-06 18:17:41 +0000

    Base data retrieve

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class DashboardController extends Controller
 {
@@ -13,8 +15,20 @@ class DashboardController extends Controller
      */
     public function index()
     {
-
+        $client = new Client(); //GuzzleHttp\Client
+        $result = $client->get('https://www.epione.oobazee.com/profile-test.json', [
+            'form_params' => [
+                'sample-form-data' => 'value'
+            ]
+        ]);
         //return response()->json('success');
-        return view('dashboard')->with('success');
+        
+        // $obj = $result->getBody();
+
+        // $obj = json_decode($result->getBody(), true);
+        
+        $obj = json_decode($result->getBody());
+
+        return view('dashboard')->with('data_p', $obj);
     }
 }
