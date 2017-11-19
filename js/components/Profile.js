6535cced8c5d7d1729983a8aa422c7334019b391
Hallam1995 <32713366+Hallam1995@users.noreply.github.com>
2017-11-19 13:49:48 +0000

    Add files via upload
    Updated web.php, css style-1 and created more interfaces

---
+++ js/components/Profile.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Tile from "../components/includes/Tile";
+import HeartRate from "../components/includes/HeartRate";
+import Allergies from "../components/includes/Allergies";
+import Symptoms from "../components/includes/Symptoms";
+import Fetch from 'react-fetch';
+let profile = require('../../profile-test.json');
+
+export default class Dashboard extends React.Component{
+render(){
+    console.log(profile);
+return(
+            <div class="container">
+    <div class="row">
+    
+        <Tile image="https://static0.fitbit.com/images/profile/defaultProfile_100_male.png" name='Name' line1={profile.user.displayName} line2={profile.user.age}/>
+        <Tile image="http://mgla.asn.au/wp-content/uploads/2017/06/icon-age.png" name='age' line1={"Age:"} line2={profile.user.age}/>
+        <Tile image="https://png.icons8.com/?id=19420&size=2x" name='Height' line1={"Current Height:"} line2={profile.user.age}/>
+        <Tile image="https://png.icons8.com/?id=37167&size=2x" name='Weight' line1={"Current Weight:"} line2={profile.user.age}/>
+    
+        <Allergies/>
+        <Symptoms/>
+
+
+            
+            <div class="mt-2 col-sm-12 col-lg-4">
+                <div class="block">
+                    <btitle>Back to Dashboard</btitle>
+                    <hr/>
+                    <div class="text-center">
+                        <i class="fa fa-cogs fa-3x" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+        );
+}
+
+ComponentDidMount() {
+// fetch('https://epione.oobazee.com/profile-test.json')
+// .then(results =>{ 
+//     return results.json();
+// }).then(data =>{
+//     let profile = data.results.map((item)=>{
+//         return(
+//             <div key={item.user.displayName}>
+//             </div>
+//             )
+//     })
+// })
+
+}
+
+}
---
+++ js/components/includes/HeartRate.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { IndexLink, Link } from "react-router";
+export default class HeartRate extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        console.log(this.props.user);
+        return(
+
+                <div class="col-sm-3 col-lg-6">
+            <div class="block">
+                <btitle>Heart Rate</btitle>
+                <hr/>
+                <div class="heart-num-block text-right">
+                    <i class="fa fa-heartbeat fa-4x" aria-hidden="true"></i>
+                    <p><bold>90</bold></p>
+                </div>
+
+                <div class="heart-rate">
+                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="150px" height="73px" viewBox="0 0 150 73" enableBackground="new 0 0 150 73">
+                        <polyline fill="none" stroke="#009B9E" strokeWidth="3" strokeMiterlimit="10" points="0,45.486 38.514,45.486 44.595,33.324 50.676,45.486 57.771,45.486 62.838,55.622 71.959,9 80.067,63.729 84.122,45.486 97.297,45.486 103.379,40.419 110.473,45.486 150,45.486" />
+                    // </svg>
+                    <div class="fade-in"></div>
+                    <div class="fade-out"></div>
+                </div>
+
+            </div>
+        </div>
+
+        );
+    }
+}
