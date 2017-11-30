c6a42036cb59216acb0b61f7b13df50fe6a83858
Hallam1995 <32713366+Hallam1995@users.noreply.github.com>
2017-11-30 11:28:46 +0000

    Add files via upload

---
+++ js/components/HeartRatePage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import { Chart } from 'react-google-charts';
+import {LineChart} from 'react-easy-chart';
+// import RTChart from 'react-rt-chart';
+let data = require('../../../test.json')["activities-heart-intraday"]["dataset"];
+
+ 
+export default class HeartRatePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+  componentDidMount() {
+    setInterval(() => {
+    var array = [];
+    for (var i = 0; i <= this.state.data.length && i<data.length; i++) {
+      array.push({"x": i,"y": data[i].value});
+    };
+    this.setState({ data: array });
+  }, 1000);
+  }
+  render() {
+    var stuff = this.state.data;
+    console.log(this.state.data);
+    return (
+      <div>
+      <LineChart
+      axes
+      width={250}
+      height={250}
+      axisLabels={{x: 'Reading', y: 'Heart Rate (BPM)'}}
+    data={[this.state.data]}/>
+            </div>
+    );
+  }
+
+}
