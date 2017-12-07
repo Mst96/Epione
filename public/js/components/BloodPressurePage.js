cb7727ff2ed456eb9dcea074dfb28f3ab5e185d6
Hallam1995 <32713366+Hallam1995@users.noreply.github.com>
2017-12-07 14:29:00 +0000

    New colour, page & graphs
    Updated tile css colour, added in temperature page and added in 'current x' to HR, BP and Temperature

--- public/js/components/BloodPressurePage.js
+++ public/js/components/BloodPressurePage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 // import { Chart } from 'react-google-charts';
 import {LineChart} from 'react-easy-chart';
+import axios from 'axios';
 // import RTChart from 'react-rt-chart';
+// let data = require('../../../test.json')["activities-heart-intraday"]["dataset"];
 let data = require('../../../bloodpressure.json')["data"];
 
  
-export default class HeartRatePage extends React.Component {
+export default class BloodPressurePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {data: [], current: 0};
@@ -14,15 +16,18 @@ export default class HeartRatePage extends React.Component {
   componentDidMount() {
     var rate;
     var array = [];
+    var d = new Date();
     for (var i = 0; i <= 5; i++) {
-      rate = Math.floor((Math.random() * 30) + 90);
-      array.push({"x": i, "y": rate});
+      var h = this.addZero(d.getUTCHours());
+      var m = this.addZero(d.getUTCMinutes() + i);
+      rate = Math.floor((Math.random() * 20) + 55);
+      array.push({"x": h + ":" + m, "y": rate});
     };
-    this.setState({ data: array });
+     this.setState({ data: array });
     var i = 5;
     setInterval(() => {
       array.push({"x": data[i].x, "y": data[i].y})
-      this.setState({ data: array });
+      this.setState({ data: array, current : data[i].y});
       i++;
   }, 3000);
   }
@@ -30,15 +35,35 @@ export default class HeartRatePage extends React.Component {
     var stuff = this.state.data;
     return (
       <div>
+      <h1>Current Blood Pressure: {this.state.current}</h1>;
       <LineChart
-      yDomainRange={[0, 200]}
+      xType={'text'}
+      yDomainRange={[0, 120]}
       axes
-      width={500}
+      dataPoints
+      width={750}
       height={500}
-      axisLabels={{x: 'Reading', y: 'Blood Pressure'}}
+      grid
+      axisLabels={{x: 'Reading', y: 'Blood Pressure:'}}
     data={[this.state.data]}/>
             </div>
     );
+  }
+  addZero(i) {
+    if(i >= 60){
+      i = i % 60;
+    }
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+  }
+
+  notify(){
+    axios.get('/api/notify')
+    .then(success =>{
+        console.log("notified");
+        });
   }
 
 }
