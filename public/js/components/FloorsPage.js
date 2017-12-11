532f725e9842f180f9a6b8ef64dd2015ce607a0d
caldwelj <jameo45@live.co.uk>
2017-12-11 02:35:12 +0000

    Sorted out distance page and changed visuals on some of the other chart pages

--- public/js/components/FloorsPage.js
+++ public/js/components/FloorsPage.js
@@ -22,7 +22,8 @@ export default class FloorPage extends React.Component {
       day: day,
       week: week,
       month: month,
-      year: year, 
+      year: year,
+      componentWidth: 300,
       current: 7.6,
       showToolTip: false,
       top: 0,
@@ -30,17 +31,35 @@ export default class FloorPage extends React.Component {
       x: 0,
       y: 0
     };
+
+    this.handleResize = this.handleResize.bind(this);
   }
+
   componentDidMount() {
-        var param = this.props.params.user;
-        var user = users.filter(function(user){
-            if(user.firstName === param){
-                console.log("YOOO MATCH");
-                return user;
-            }
-        })
-        this.setState({user: user[0]});
-    }
+    var param = this.props.params.user;
+    var user = users.filter(function(user){
+      if(user.firstName === param){
+        console.log("YOOO MATCH");
+        return user;
+      }
+    })
+
+    this.setState({user: user[0]});
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      windowWidth: window.innerWidth - 100,
+      componentWidth: this.myInput.offsetWidth
+    });
+  }
+
   render() {
     var stuff = this.state.data;
     var tooltip = null;
@@ -48,9 +67,13 @@ export default class FloorPage extends React.Component {
       tooltip = this.selected();
     }
     return (
-      <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-lg-12" ref="block">
+            <div class="block" ref={input => {this.myInput = input}}>
+              <btitle>Floors Climbed</btitle>
         <h1>{this.state.user.firstName} has climbed {this.state.current} floors today</h1>;
-        {tooltip}
+        
         <link href="../../../css/mui.min.css" rel="stylesheet" type="text/css" media="screen" />
         <Tabs justified={true} onChange={this.onChange}>
           <Tab value="Past Day" label="Past Day">
@@ -58,10 +81,10 @@ export default class FloorPage extends React.Component {
             axes
             colorBars
             barWidth = {15}
-            height={500}
-            width={500}
             margin={{ top: 50, right: 0, bottom: 50, left: 80 }}
-            axisLabels={{x: 'Reading', y: 'Floors'}}
+            width = {this.state.componentWidth - 80}
+            height = {this.state.componentWidth / 2}
+            axisLabels={{x: 'Hour', y: 'Floors Climbed'}}
             mouseOverHandler={this.mouseOverHandler}
             data={this.state.day}/>
             </Tab>
@@ -70,10 +93,10 @@ export default class FloorPage extends React.Component {
             axes
             colorBars
             barWidth = {15}
-            height={500}
-            width={500}
             margin={{ top: 50, right: 0, bottom: 50, left: 80 }}
-            axisLabels={{x: 'Reading', y: 'Floors'}}
+            width = {this.state.componentWidth - 80}
+            height = {this.state.componentWidth / 2}
+            axisLabels={{x: 'Day', y: 'Floors Climbed'}}
             mouseOverHandler={this.mouseOverHandler}
             data={this.state.week}/>
             </Tab>
@@ -82,10 +105,10 @@ export default class FloorPage extends React.Component {
             axes
             colorBars
             barWidth = {15}
-            height={500}
-            width={500}
             margin={{ top: 50, right: 0, bottom: 50, left: 80 }}
-            axisLabels={{x: 'Reading', y: 'Floors'}}
+            width = {this.state.componentWidth - 80}
+            height = {this.state.componentWidth / 2}
+            axisLabels={{x: 'Day', y: 'Floors Climbed'}}
             mouseOverHandler={this.mouseOverHandler}
             data={this.state.month}/>
             </Tab>
@@ -94,20 +117,38 @@ export default class FloorPage extends React.Component {
             axes
             colorBars
             barWidth = {15}
-            height={500}
-            width={500}
             margin={{ top: 50, right: 0, bottom: 50, left: 80 }}
-            axisLabels={{x: 'Reading', y: 'Floors'}}
+            width = {this.state.componentWidth - 80}
+            height = {this.state.componentWidth / 2}
+            axisLabels={{x: 'Month', y: 'Floors Climbed'}}
             mouseOverHandler={this.mouseOverHandler}
             data={this.state.year}/>
             </Tab>
         </Tabs>
+            </div>
+          </div>
+        </div>
+        {tooltip}
       </div>
     );
   }
   selected(){
-    return (<h1>At {this.state.x}, {this.state.user.firstName} climbed {this.state.y} floors</h1>);
-  }
+    if( this.state.x == 1 || this.state.x == 21 || this.state.x == 31  )
+    {
+      return (<h1>On the {this.state.x}st, {this.state.user.firstName} climbed {this.state.y} floors</h1>);
+    } else if( this.state.x == 2 || this.state.x == 22 )
+    {
+      return (<h1>On the {this.state.x}nd, {this.state.user.firstName} climbed {this.state.y} floors</h1>);
+    } else if( this.state.x == 3 || this.state.x == 23 )
+    {
+      return (<h1>On the {this.state.x}rd, {this.state.user.firstName} climbed {this.state.y} floors</h1>);
+    } else
+    {
+      return (<h1>On the {this.state.x}th, {this.state.user.firstName} climbed {this.state.y} floors</h1>);
+    }
+    
+    }
+
   addZero(i) {
     if(i >= 60){
       i = i % 60;
